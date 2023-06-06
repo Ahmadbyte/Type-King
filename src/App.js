d184838c7c28ed4218f48e78396743a86757e22a
Ahmadbyte <shariqueahmad011@gmail.com>
2023-06-06 11:49:56 +0530

    device2

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function App() {
 
   return (
     <div className="App" >
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       <div className="is-size">
         Time Remaining:
         <br />
