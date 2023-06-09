fef688128c2e6eab858ca5720ad3b5669995c78e
Ahmadbyte <shariqueahmad011@gmail.com>
2023-06-09 11:50:57 +0530

    changed name and added logo

---
+++ README.md
@@ -0,0 +1,14 @@
+Type King:
+
+firstly i created the file with the following:
+{npx-create-react-app touch-typing-app}
+
+then created the two pages named are:
+{Start page} in which when user will open the website then they will see the first page.
+
+In Start page it will contain the start button which will redirect them to the start page in which they can start the typing test.
+
+Then i created the secong page named:
+{Typing Page} in which user can start there typing test
+
+This typing page will contain the 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Login from './pages/login/Login'
 import Start from './pages/start/Start'
+import Type from './pages/type/Type'
 import { BrowserRouter, Route , Routes} from 'react-router-dom'
 
 
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={< Login />} />
-      <Route exact path="/start" element={< Start />} />
+      <Route exact path="/" element={< Start />} />
+      <Route exact path="/type" element={< Type />} />
     </Routes>
   </BrowserRouter>
   )
